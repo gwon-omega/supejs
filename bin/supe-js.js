128c8f76f07d5de2a45119beb7334a33eeea151e
Jeevan Bhatt <228005501+gwon-omega@users.noreply.github.com>
2026-02-27 19:07:02 +0545

    Simplify CLI bin aliases and drop legacy wrapper files

--- bin/supe-js.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { main } from "./supe.js";
-
-try {
-  process.exitCode = main(process.argv.slice(2));
-} catch (error) {
-  console.error(error.message);
-  process.exitCode = 1;
-}
